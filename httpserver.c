bc8309b00546378bfb268bff4855591362505152
Kamil <kamkalet@gmail.com>
2017-12-11 22:22:03 +0000

    starting implementing sending via socket part1

--- httpserver.c
+++ httpserver.c
@@ -25,32 +25,21 @@ struct in6_addr ip6_src;	/* source address */
 struct in6_addr ip6_dst;	/* destination address */
 
 void service_error(char *message);
-void process_packet(unsigned char* buffer, int size);
+void process_packet(unsigned char* buffer, int size, int dsc);
 void print_ethernet_header(unsigned char* buffer, int size);
 void print_ip6_header(unsigned char* buffer, int size);
 void print_icmp6_packet(unsigned char* buffer , int size);
 void print_tcp_packet(unsigned char* buffer , int size);
+int create_socket();
+void send_icmp6_answer(buffer);
 
 int main (int argc, char **argv)
 {
 	printf("Starting server\n");
 
-	struct ifreq ifr;
-	int socket_desc;
+	int socket_desc_rcv = create_socket();
+	int socket_desc_send = create_socket();
 	unsigned char *buffer;
-
-	// create raw socket for basic packets and sniffing ethernet packets
-	if (((socket_desc = socket (AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0)) 
-		service_error("Error receiving listening socket desc: ");
-		
-	// binding socket to interface etc0
-	memset(&ifr, 0, sizeof(struct ifreq));
-	snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), "eth0");
-	ioctl(socket_desc, SIOCGIFINDEX, &ifr);
-	char *devname = "eth0";
-	if(setsockopt(socket_desc, SOL_SOCKET, SO_BINDTODEVICE,  devname, 10)<0){
-		service_error("Error binding");
-	}
 
 
 	printf("Starting receiving...\n");
@@ -59,13 +48,13 @@ int main (int argc, char **argv)
 	buffer = (unsigned char *) malloc (65536* sizeof (unsigned char));
 	//RECEIVING
 	while(1){
-		if ((bytes = recvfrom (socket_desc, (void*)buffer, 65536, 0, NULL, (socklen_t*)sizeof (struct sockaddr_in6))) < 0)  {
+		if ((bytes = recvfrom (socket_desc_rcv, (void*)buffer, 65536, 0, NULL, (socklen_t*)sizeof (struct sockaddr_in6))) < 0)  {
 			service_error("recvfrom() failed ");
 		}else{
 			//printf("succes\n");
 			//printf("Number of bytes: %d", bytes);
 			//printf("\n");
-			process_packet(buffer, bytes);
+			process_packet(buffer, bytes, socket_desc_send);
 		}
 	}
 	
@@ -81,7 +70,30 @@ void service_error(char *message){
 	
 }
 
-void process_packet(unsigned char* buffer, int size)
+int create_socket(){
+	
+	int socket_desc;
+	struct ifreq ifr;
+	
+	// create raw socket for basic packets and sniffing ethernet packets
+	if (((socket_desc = socket (AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0)) 
+		service_error("Error receiving listening socket desc: ");
+		
+	// binding socket to interface etc0
+	memset(&ifr, 0, sizeof(struct ifreq));
+	snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), "eth0");
+	ioctl(socket_desc, SIOCGIFINDEX, &ifr);
+	
+	char *devname = "eth0";
+	if(setsockopt(socket_desc, SOL_SOCKET, SO_BINDTODEVICE,  devname, 4)<0){
+		service_error("Error binding");
+	}
+	
+	return socket_desc;
+	
+}
+
+void process_packet(unsigned char* buffer, int size, int dsc)
 {
 
 	//printf("%d",sizeof(struct ethhdr));
@@ -93,6 +105,7 @@ void process_packet(unsigned char* buffer, int size)
     {
         case 58:  //ICMP Protocol
             print_icmp6_packet( buffer , size);
+            send_icmp6_answer(buffer. dsc);
             break;
 
         case 6:  //TCP Protocol
@@ -103,6 +116,56 @@ void process_packet(unsigned char* buffer, int size)
             break;
     }
 
+}
+
+void send_icmp6_answer(unsigned char* buffer, int socket_desc){
+	
+	struct ethhdr *eth = (struct ethhdr *)buffer;
+	struct ip6_hdr *ip6h = (struct ip6_hdr *)(buffer + sizeof(struct ethhdr));
+	struct icmp6_hdr *icmph6 = (struct icmp6_hdr *)(buffer + ip6_hdrlen  + sizeof(struct ethhdr));
+	
+	//packet to be sent
+	unsigned char packet[4096];
+	memset (packet, 0, 4096);
+	
+	struct ethhdr *eth_send = (struct ethhdr *)packet;
+	struct ip6_hdr *ip6h_send = (struct ip6_hdr *)(packet + sizeof(struct ethhdr));
+	struct icmp6_hdr *icmph6_send = (struct icmp6_hdr *)(packet + 40 + sizeof(struct ethhdr));
+	
+	memcpy(eth_send->h_dest, eth->h_source, ETH_ALEN);
+	memcpy(eth_send->h_source, eth->h_dest, ETH_ALEN);
+	memcpy(eth_send->h_proto, eth->h_proto, sizeof __be16);
+	
+	memcpy(packet, eth, sizeof(struct ethhdr));
+	
+	memcpy(ip6h_send->ip6_ctlun.ip6_un1.ip6_un1_flow, ip6h->ip6_ctlun.ip6_un1.ip6_un1_flow, sizeof(uint32_t));
+	memcpy(ip6h_send->ip6_ctlun.ip6_un1.ip6_un1_plen, ip6h->ip6_ctlun.ip6_un1.ip6_un1_plen, sizeof(uint16_t));
+	memcpy(ip6h_send->ip6_ctlun.ip6_un1.ip6_un1_nxt, ip6h->ip6_ctlun.ip6_un1.ip6_un1_nxt, sizeof(uint8_t));
+	memcpy(ip6h_send->ip6_ctlun.ip6_un1.ip6_un1_hlim, ip6h->ip6_ctlun.ip6_un1.ip6_un1_hlim, sizeof(uint8_t));
+	memcpy(ip6h_send->ip6_ctlun.ip6_un2_vfc, ip6h->ip6_ctlun.ip6_un2_vfc, sizeof(uint8_t));
+	memcpy(ip6h_send->ip6src, ip6h->ip6_ctlun.ip6_un2_vfc, sizeof(uint8_t));
+	memcpy(ip6h_send->ip6_ctlun.ip6_dst, ip6h->ip6_ctlun.ip6_dst, sizeof(struct in6_addr));
+	memcpy(ip6h_send->ip6_ctlun.ip6_dst, ip6h->ip6_ctlun.ip6_dst, sizeof(struct in6_addr));
+	
+	
+	
+	eth->h_dest[0] , eth->h_dest[1] , eth->h_dest[2] , eth->h_dest[3] , eth->h_dest[4] , eth->h_dest[5] );
+    printf("   |-Source Address      : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X \n", eth->h_source[0] , eth->h_source[1] , eth->h_source[2] , eth->h_source[3] , eth->h_source[4] , eth->h_source[5] );
+    printf("   |-Protocol            : %u \n",(unsigned short)eth->h_proto);
+	  printf("   |-Version,class,flow: %u\n", (unsigned int)(ip6h->ip6_ctlun.ip6_un1.ip6_un1_flow));
+    printf("   |-Payload length    : %u\n",(unsigned int)(ip6h->ip6_ctlun.ip6_un1.ip6_un1_plen));
+    printf("   |-Next header       : %u\n", (unsigned int)(ip6h->ip6_ctlun.ip6_un1.ip6_un1_nxt));
+    printf("   |-Hop limit         : %u\n",(unsigned int)ip6h->ip6_ctlun.ip6_un1.ip6_un1_hlim);
+	
+	
+	
+	
+	
+	if (sendto (socket_desc, packet, iph->tot_len ,  0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
+    {
+		service_error("sendto failed");
+    }
+	
 }
 
 void print_ethernet_header(unsigned char* buffer, int size)
